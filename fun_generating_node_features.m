05e2b56f8dd41f98175d87aa64ac6bc1b0643d74
Mohammad Esmaeili <47409273+mohammadesmaeili@users.noreply.github.com>
2020-10-16 15:44:26 -0500

    Delete fun_generating_node_features.m

--- fun_generating_node_features.m
+++
@@ -1,9 +0,0 @@
-function [X, Y] = fun_generating_node_features(n,rho, alpha)
-K=ceil(n*rho);
-X=[ones(K,1);-ones(n-K,1)];
-permx = randperm(n);
-X=X(permx);
-R = 2*binornd(1,1-alpha,n,1)-1;
-Y = R.*X;
-end
-
